28f117a11681f7f16a1a23f2f2a95027fa2cb950
david <david@qq.com>
2026-03-05 16:57:47 +0800

    fix: Twitter 用户同步间隔改为 2 分钟

--- backend/src/index.js
+++ backend/src/index.js
@@ -56,7 +56,7 @@ const twitterRouter = (await import('./routes/twitter.js')).default;
 app.use('/api/twitter', twitterRouter);
 
 startTwitterSync(300000); // 每 5 分钟同步一次
-startTwitterUserSync(300000); // 每 5 分钟同步一次 Twitter 用户推文
+startTwitterUserSync(120000); // 每 2 分钟同步一次 Twitter 用户推文
 
 // 根路径
 app.get('/', (req, res) => {
